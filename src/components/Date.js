e7a13fccc07ea2d3a6bc60e3aaa99bd32e8585f5
fatma-brahmi <brahmifatma96@gmail.com>
2021-09-21 14:55:51 +0100

    react-router

---
+++ src/components/Date.js
@@ -0,0 +1,63 @@
+export const movies = [
+  {
+    Title: "Parasite",
+    Poster:
+      "https://static.cinebel.be/img/movie/poster/full/1020491_fr_parasite_1563531796112.jpg",
+    Year: 2019,
+    rating: 4,
+    video: "https://www.youtube.com/embed/tsow4PaUyo8",
+    id: 1,
+    desc:"Toute la famille de Ki-taek est au chômage, et s'intéresse fortement au train de vie de la richissime famille Park, jusqu'à ce qu'ils soient impliqués dans un incident inattendu."
+  },
+  {
+    Title: "Adieu les cons ",
+    Poster:
+      "https://media.senscritique.com/media/000020053720/100/Adieu_les_cons.jpg",
+    Year: 2020,
+    rating: 4,
+    video: "https://www.youtube.com/embed/9uotsv-vf6I",
+    id: 2,
+    desc:"Atteinte d’une maladie incurable, une femme cherche à retrouver l’enfant à qui elle a donné naissance alors qu’elle était âgée de 15 ans et qu’elle a été forcée d’abandonner. L’appuieront dans sa quête un fonctionnaire dépressif ayant raté sa tentative de suicide et un archiviste aveugle…",
+  },
+  {
+    Title: "Antoinette dans les Cévennes",
+    Poster:
+      "https://media.senscritique.com/media/000019573100/100/Antoinette_dans_les_Cevennes.jpg",
+    Year: 2019,
+    rating: 3,
+    video: "https://www.youtube.com/embed/Yqyw3sH666o",
+    id: 3,
+    desc:" le nouveau film de Caroline Vignal, est attendu au cinéma le 16 septembre 2020. Cette comédie très réussie met en scène Laura Calamy, Olivia Côte et Benjamin Lavernhe.",
+  },
+
+  {
+    Title: " Été 85 ",
+    Year: "2021",
+    imdbID: "tt2488496",
+    video: "https://www.youtube.com/embed/ZG0tRdHpCBk",
+    Poster: "https://media.senscritique.com/media/000019445041/100/Ete_85.jpg",
+    rating: 5,
+    id: 4,
+    desc:"Le film est construit sur des allées et venues entre le temps des faits et celui du récit qu'Alex en fait, à la suggestion de son professeur de lettres, en vue de sa comparution devant la justice.",
+  },
+  {
+    rating: 3,
+    Title: " Tout simplement noir",
+    Year: "2021",
+    id: 5,
+desc:"JP, un acteur raté de 40 ans, décide d’organiser la première grosse marche de contestation noire en France, mais ses rencontres, souvent burlesques, avec des personnalités influentes de la communauté et le soutien intéressé qu’il reçoit de Fary, le font osciller entre envie d’être sur le devant de la scène et véritable engagement militant",
+    video: "https://www.youtube.com/embed/2Uk15jDnJz0",
+    Poster:
+      "https://media.senscritique.com/media/000019226654/100/Tout_simplement_noir.jpg",
+  },
+
+  {
+    rating: 5,
+    id: 6,
+    Title: " Mine de Rien",
+    Year: "2019",
+    video: "https://www.youtube.com/embed/MWR8RVSS-DM",
+    Poster: "https://p5.storage.canalblog.com/58/87/849913/126516074_o.jpg",
+    desc: "Le scénario de Mine de rien rappelle beaucoup l'histoire de Bud Hurlbut, un Américain qui a eu une idée géniale dans les années 1950 : pour faire patienter les clients d'un restaurant qui connaissait un franc succès, il leur offrait des tours de wagonnets dans l'ancienne mine d'or toute proche. Bien avant Disney, Bud Hurlbut développa ensuite un parc d'attraction autour de la mine.",
+  },
+];
